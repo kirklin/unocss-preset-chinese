a5a6f7548ceb765b6e6977c758ca2891a2e4b1e4
Kirk Lin <linkirk@163.com>
2023-08-30 20:26:51 +0800

    refactor: dynamic generation font and CSS rules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { toArray } from "@unocss/core";
 import type { Preset } from "unocss";
-import { defaultChineseFonts } from "./defaultChineseFonts";
+import type { FontType } from "./defaultChineseFonts";
+import { defaultChineseFonts, generateFontFaceRule } from "./defaultChineseFonts";
 import type { ChineseFontsOptions } from "./types";
 
 export * from "./types";
@@ -43,24 +44,12 @@ const presetChinese = (options: ChineseFontsOptions = {}): Preset => {
       }
     };
   }
-  //  TODO: Dynamically calculates and sets the font styles for specific Chinese characters based on the selected theme's fonts.
-  //  TODO: 根据所选主题的字体动态计算并设置特定中文字符的字体样式。
-  preset.preflights = [
-    {
-      getCSS: () => `
-      @font-face {
-        font-family: 'Punctuation SC';
-        src: local('PingFang SC'), local('Noto Sans SC'), local('Noto Sans CJK SC'), local('Heiti SC'), local('Microsoft Yahei');
-        unicode-range: U+201C, U+201D, U+2018, U+2019, U+2E3A, U+2014, U+2013, U+2026, U+00B7, U+007E, U+002F; /* Unicode range for punctuation marks */
-      }
-      @font-face {
-        font-family: 'Punctuation TC';
-        src: local('PingFang TC'), local('Noto Sans TC'), local('Noto Sans CJK TC'), local('Heiti TC'), local('Microsoft JhengHei');
-        unicode-range: U+201C, U+201D, U+2018, U+2019, U+2E3A, U+2014, U+2013, U+2026, U+00B7, U+007E, U+002F; /* Unicode range for punctuation marks */
-      }
-    `,
-    },
-  ];
+
+  const fontFacePreflights = Object.keys(defaultChineseFonts).map(fontType => ({
+    getCSS: () => generateFontFaceRule(<FontType>fontType),
+  }));
+
+  preset.preflights = fontFacePreflights;
 
   return preset;
 };
